2fa5b85c167ae9fd53e6273f613739f562bfab9d
LewinsHp <lewinsanhamonanganpanjaitan@gmail.com>
2023-03-31 08:57:26 +0700

    Homehandler

--- main.go
+++ main.go
@@ -5,3 +5,16 @@ import (
 	"net/http"
 	"v2/handler"
 )
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handler.HomeHandler)
+	mux.HandleFunc("/ga", handler.GaHandler)
+	mux.HandleFunc("/ber", handler.BerHandler)
+	mux.HandleFunc("/product", handler.ProductHandler)
+	mux.HandleFunc("/post-get", handler.PostGet)
+	mux.HandleFunc("/form", handler.Form)
+	mux.HandleFunc("/process", handler.Process)
+
+}
