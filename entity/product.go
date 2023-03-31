91cc4ed1534cb590a5ba007845928d1583ab493b
LewinsHp <lewinsanhamonanganpanjaitan@gmail.com>
2023-03-31 08:53:27 +0700

    stockstatus

--- entity/product.go
+++ entity/product.go
@@ -6,3 +6,15 @@ type Product struct {
 	Price int
 	Stock int
 }
+
+func (p Product) StockStatus() string {
+	var status string
+
+	if p.Stock < 3 {
+		status = "Stock Hampir Habis"
+	} else if p.Stock < 10 {
+		status = "Stock Terbatas"
+	}
+
+	return status
+}
